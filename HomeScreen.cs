70197536111a11f39154b395f7cba3abbf9c9811
Unknown <andreaskuuskaru99@hotmail.com>
2018-05-24 23:16:52 +0300

    commit

---
+++ HomeScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace CustomRowView {
+    [Activity(Label = "Autod", MainLauncher = true, Icon = "@drawable/icon")]
+    public class HomeScreen : Activity{//, View.IOnClickListener {
+
+        List<TableItem> tableItems = new List<TableItem>();
+        ListView listView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.HomeScreen);
+            listView = FindViewById<ListView>(Resource.Id.List);
+
+            tableItems.Add(new TableItem() { Heading = "AUDI", SubHeading = "650 kw", ImageResourceId = Resource.Drawable.logo});
+            tableItems.Add(new TableItem() { Heading = "quattro", SubHeading = "170 kw", ImageResourceId = Resource.Drawable.quattro });
+            tableItems.Add(new TableItem() { Heading = "RS2", SubHeading = "50 kw", ImageResourceId = Resource.Drawable.RS2 });
+            tableItems.Add(new TableItem() { Heading = "RS6", SubHeading = "330 kw", ImageResourceId = Resource.Drawable.RS6 });
+            tableItems.Add(new TableItem() { Heading = "R8", SubHeading = "180 kw", ImageResourceId = Resource.Drawable.R8 });
+         
+
+            listView.Adapter = new HomeScreenAdapter(this, tableItems);
+
+            listView.ItemClick += OnListItemClick;
+        }
+
+        protected void OnListItemClick(object sender, Android.Widget.AdapterView.ItemClickEventArgs e)
+        {
+            var listView = sender as ListView;
+            var t = tableItems[e.Position];
+            Android.Widget.Toast.MakeText(this, t.Heading+"("+t.SubHeading+")", Android.Widget.ToastLength.Short).Show();
+            Console.WriteLine("Klikkisid " + t.Heading);
+        }
+    }
+}
---
+++ obj/Debug/android/src/md5d8359e76a35968359354b626b6df299b/HomeScreen.java
@@ -0,0 +1,47 @@
+package md5d8359e76a35968359354b626b6df299b;
+
+
+public class HomeScreen
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("CustomRowView.HomeScreen, CustomRowView, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", HomeScreen.class, __md_methods);
+	}
+
+
+	public HomeScreen ()
+	{
+		super ();
+		if (getClass () == HomeScreen.class)
+			mono.android.TypeManager.Activate ("CustomRowView.HomeScreen, CustomRowView, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
